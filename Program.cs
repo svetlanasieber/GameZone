bad952ce219d19b1412442d4252f775d47e1a625
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-06-13 23:14:31 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace GameZone
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+           
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<GameZoneDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -37,7 +37,7 @@ namespace GameZone
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+    
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -45,7 +45,7 @@ namespace GameZone
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+           
                 app.UseHsts();
             }
 
